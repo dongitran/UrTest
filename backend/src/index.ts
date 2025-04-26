9a2dd6ce0342c66be9a8ab037f2ed69e9eafa44f
hieudien14310 <tranhieu14310@gmail.com>
2025-04-26 19:45:27 +0700

    add ProjectRoute with GET and POST endpoints for project management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,18 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
 import MongoConfig from "./config/mongodb";
+import ProjectRoute from "@route/project.route";
+import VerifyToken from "@middlewars/VerifyToken";
+
 const app = new Hono();
 
 MongoConfig.connectToMongoDB();
 
 app.use(cors());
 app.use(logger(customLogger));
+app.use("/api/*", VerifyToken());
+
+app.route("/api/project", ProjectRoute);
 
 app.onError(ErrorLog);
 
---
+++ backend/src/routes/project.route.ts
@@ -0,0 +1,36 @@
+import { zValidator } from "@hono/zod-validator";
+import dayjs from "dayjs";
+import db from "db/db";
+import { ProjectTable } from "db/schema";
+import { Hono } from "hono";
+import { ulid } from "ulid";
+import { z } from "zod";
+
+const ProjectRoute = new Hono();
+ProjectRoute.get("/", async (ctx) => {
+  return ctx.json({ projects: [] });
+});
+ProjectRoute.post(
+  "/",
+  zValidator(
+    "json",
+    z.object({
+      title: z.string(),
+      description: z.string(),
+    })
+  ),
+  async (ctx) => {
+    const body = ctx.req.valid("json");
+    const user = ctx.get("user");
+
+    await db.insert(ProjectTable).values({
+      createdAt: dayjs().toISOString(),
+      description: body.description,
+      id: ulid(),
+      title: body.title,
+      createdBy: user.id,
+    });
+    return ctx.json({ message: "ok" });
+  }
+);
+export default ProjectRoute;
