0d51db6ba9db9bf8d48682eaec4936cce55e59ef
hieudien14310 <tranhieu14310@gmail.com>
2025-04-27 13:20:43 +0700

    fix: correct project query condition and restructure response data format in DashboardRoute

--- backend/src/routes/dashboard.route.ts
+++ backend/src/routes/dashboard.route.ts
@@ -4,13 +4,23 @@ import { Hono } from "hono";
 const DashboardRoute = new Hono();
 DashboardRoute.get("/", async (ctx) => {
   const projects = await db.query.ProjectTable.findMany({
-    where: (clm, { isNotNull }) => isNotNull(clm.deletedAt),
+    where: (clm, { isNull }) => isNull(clm.deletedAt),
   });
+  const dataTable = [];
+  for (const item of projects) {
+    dataTable.push({
+      title: item.title,
+      description: item.description,
+      status: "Stable",
+      updatedAt: item.updatedAt,
+    });
+  }
   return ctx.json({
     totalProject: projects.length,
     totalTestcase: 0,
     totalAvgSuccessRate: 0,
     totalActive: 0,
+    dataTable,
   });
 });
 export default DashboardRoute;
