0a6c68329dfaf61648acfb2256239f8a58bdcfa7
hieudien14310 <tranhieu14310@gmail.com>
2025-04-28 18:59:14 +0700

    refactor: enhance error handling in TestSuiteRoute for better user feedback

--- backend/src/routes/testsuite.route.ts
+++ backend/src/routes/testsuite.route.ts
@@ -8,6 +8,25 @@ import { ulid } from "ulid";
 import { z } from "zod";
 
 const TestSuiteRoute = new Hono();
+TestSuiteRoute.get(
+  "/:id",
+  zValidator(
+    "param",
+    z.object({
+      id: z.string().ulid(),
+    })
+  ),
+  async (ctx) => {
+    const { id } = ctx.req.valid("param");
+    const testSuite = await db.query.TestSuiteTable.findFirst({
+      where: (clm, { eq, and, isNull }) => and(eq(clm.id, id), isNull(clm.deletedAt)),
+    });
+    if (!testSuite) {
+      return ctx.json({ message: "Không tìm thấy thông tin kịch bản test" }, 404);
+    }
+    return ctx.json({ ...testSuite });
+  }
+);
 TestSuiteRoute.post(
   "/",
   zValidator(
@@ -36,7 +55,48 @@ TestSuiteRoute.post(
     return ctx.json({ message: "ok" });
   }
 );
-
+TestSuiteRoute.patch(
+  "/:id",
+  zValidator(
+    "param",
+    z.object({
+      id: z.string().ulid(),
+    })
+  ),
+  zValidator(
+    "json",
+    z.object({
+      projectId: z.string().ulid(),
+      name: z.string(),
+      description: z.string(),
+      content: z.string(),
+      tags: z.array(z.string()).optional(),
+    })
+  ),
+  async (ctx) => {
+    const { id } = ctx.req.valid("param");
+    const user = ctx.get("user");
+    const body = ctx.req.valid("json");
+    const testSuite = await db.query.TestSuiteTable.findFirst({
+      where: (clm, { eq, and, isNull }) => and(eq(clm.id, id), isNull(clm.deletedAt)),
+    });
+    if (!testSuite) {
+      return ctx.json({ message: "Không tìm thấy thông tin kịch bản test" }, 404);
+    }
+    await db
+      .update(TestSuiteTable)
+      .set({
+        updatedAt: dayjs().toISOString(),
+        updatedBy: user.email,
+        content: body.content,
+        description: body.description,
+        name: body.name,
+        tags: body.tags,
+      })
+      .where(eq(TestSuiteTable.id, testSuite.id));
+    return ctx.json({ message: "ok" });
+  }
+);
 TestSuiteRoute.delete(
   "/:id",
   zValidator(
