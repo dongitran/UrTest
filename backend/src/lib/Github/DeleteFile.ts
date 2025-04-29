46929d71c35d1e66945b38e562696865c2c7c926
hieudien14310 <tranhieu14310@gmail.com>
2025-04-29 16:49:09 +0700

    feat: implement DeleteFileFromGithub function to remove files from GitHub

---
+++ backend/src/lib/Github/DeleteFile.ts
@@ -0,0 +1,29 @@
+import CheckFileFromGithub from "./CheckFile";
+
+/**
+ * Hàm dùng để xóa file trên Github
+ * @param param0
+ */
+export async function DeleteFileFromGithub({
+  fileName,
+  projectSlug,
+}: {
+  projectSlug: string;
+  fileName: string;
+}): Promise<void> {
+  const fileData = await CheckFileFromGithub({ projectSlug, fileName });
+  if (fileData) {
+    await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${fileName}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${Bun.env.GITHUB_TOKEN}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+      body: JSON.stringify({
+        message: "Delete old file",
+        branch: "main",
+        sha: fileData.sha,
+      }),
+    });
+  }
+}
