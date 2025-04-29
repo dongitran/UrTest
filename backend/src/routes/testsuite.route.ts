3528f9a5b892037ac9e8b0cd8d12ec7b91b55117
hieudien14310 <tranhieu14310@gmail.com>
2025-04-29 16:18:44 +0700

    feat: enhance PATCH endpoint to validate project existence and update test suite with GitHub SHA

--- backend/src/routes/testsuite.route.ts
+++ backend/src/routes/testsuite.route.ts
@@ -5,6 +5,7 @@ import { TestSuiteTable } from "db/schema";
 import { eq } from "drizzle-orm";
 import CreateOrUpdateTestSuiteFile from "handler/create-new-testsuite-file";
 import { Hono } from "hono";
+import { get } from "lodash";
 import { ulid } from "ulid";
 import { z } from "zod";
 
@@ -113,13 +114,28 @@ TestSuiteRoute.patch(
     const { id } = ctx.req.valid("param");
     const user = ctx.get("user");
     const body = ctx.req.valid("json");
+
+    const project = await db.query.ProjectTable.findFirst({
+      where: (clm, { eq }) => eq(clm.id, body.projectId),
+    });
+    if (!project) {
+      return ctx.json({ message: "Thông tìm thấy thông tin của Project" }, 404);
+    } else if (project.deletedAt) {
+      return ctx.json({ message: "Project đã bị xóa nên không thể tạo kịch bản test" }, 400);
+    }
+
     const testSuite = await db.query.TestSuiteTable.findFirst({
       where: (clm, { eq, and, isNull }) => and(eq(clm.id, id), isNull(clm.deletedAt)),
     });
     if (!testSuite) {
       return ctx.json({ message: "Không tìm thấy thông tin kịch bản test" }, 404);
     }
-    await db
+    const sha = get(testSuite, "params.githubData.content.sha");
+    if (!sha) {
+      return ctx.json({ message: "Không tìm thấy SHA để có thể động bộ sang UrTest Workflow Github" }, 400);
+    }
+
+    const testSuiteUpdated = await db
       .update(TestSuiteTable)
       .set({
         updatedAt: dayjs().toISOString(),
@@ -129,7 +145,30 @@ TestSuiteRoute.patch(
         name: body.name,
         tags: body.tags,
       })
-      .where(eq(TestSuiteTable.id, testSuite.id));
+      .where(eq(TestSuiteTable.id, testSuite.id))
+      .returning()
+      .then((res) => res[0]);
+    if (project.slug && testSuite.content && testSuiteUpdated.content) {
+      CreateOrUpdateTestSuiteFile(
+        {
+          projectSlug: project.slug,
+          testSuiteContent: testSuiteUpdated.content,
+          testSuiteName: `${testSuiteUpdated.id}-${testSuiteUpdated.fileName}.robot`,
+          sha,
+        },
+        async (data: Record<string, any>) => {
+          await db
+            .update(TestSuiteTable)
+            .set({
+              params: {
+                ...(testSuite.params || {}),
+                githubData: data,
+              },
+            })
+            .where(eq(TestSuiteTable.id, testSuiteUpdated.id));
+        }
+      );
+    }
     return ctx.json({ message: "ok" });
   }
 );
