d1dfaf84f79e0bb0f03be8f96429928cf75b84fa
hieudien14310 <tranhieu14310@gmail.com>
2025-04-29 16:49:03 +0700

    refactor: rename CheckTestSuiteFile to CheckFileFromGithub and update parameter names for clarity

--- backend/src/lib/Github/CheckFile.ts
+++ backend/src/lib/Github/CheckFile.ts
@@ -1,11 +1,16 @@
-export default async function CheckTestSuiteFile({
+/**
+ * Hàm dùng để kiểm tra file có tồn tại không
+ * @param param0
+ * @returns
+ */
+export default async function CheckFileFromGithub({
   projectSlug,
-  testSuiteName,
+  fileName,
 }: {
   projectSlug: string;
-  testSuiteName: string;
+  fileName: string;
 }) {
-  const checkRes = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${testSuiteName}`, {
+  const checkRes = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${fileName}`, {
     headers: {
       Authorization: `Bearer ${Bun.env.GITHUB_TOKEN}`,
       Accept: "application/vnd.github.v3+json",
