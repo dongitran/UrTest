7e4f989bd1eab488fdfcac8a0efa6f12e57672c2
hieudien14310 <tranhieu14310@gmail.com>
2025-04-29 16:55:05 +0700

    refactor: rename CreateOrUpdateTestSuiteFile to CreateOrUpdateFile and update parameters for consistency

--- backend/src/lib/Github/CreateOrUpdateFile.ts
+++ backend/src/lib/Github/CreateOrUpdateFile.ts
@@ -2,29 +2,29 @@
  * Hàm dùng để tạo testsuite file cho UrTet Workflow (repository)
  * @param testSuiteName : testSuiteId + fileName. Ví dụ: 01JSVGHZ1XJNAVCBPZPB4QW57T-test1.robot
  */
-export default async function CreateOrUpdateTestSuiteFile(
+export default async function CreateOrUpdateFile(
   {
     projectSlug,
-    testSuiteContent,
-    testSuiteName,
+    fileContent,
+    fileName,
     sha,
   }: {
     projectSlug: string;
-    testSuiteName: string;
-    testSuiteContent: string;
+    fileName: string;
+    fileContent: string;
     sha?: string;
   },
   callback?: (data: Record<string, any>) => void
 ): Promise<void> {
-  const response = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${testSuiteName}`, {
+  const response = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${fileName}`, {
     method: "PUT",
     headers: {
       Authorization: `Bearer ${Bun.env.GITHUB_TOKEN}`,
       Accept: "application/vnd.github.v3+json",
     },
     body: JSON.stringify({
-      message: "Create new testsuite file",
-      content: Buffer.from(testSuiteContent).toString("base64"),
+      message: `${sha ? "Update" : "Create"} ${fileName} testsuite file`,
+      content: Buffer.from(fileContent).toString("base64"),
       branch: "main",
       sha, //? Nếu không có SHA thì là tạo mới, có thì là update
     }),
