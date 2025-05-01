f26e00fc31cba035c126b396acc86d9097c58ab2
hieudien14310 <tranhieu14310@gmail.com>
2025-05-01 13:48:07 +0700

    feat: update dashboard route and stats to reflect total test suites

--- backend/src/routes/dashboard.route.ts
+++ backend/src/routes/dashboard.route.ts
@@ -1,4 +1,6 @@
 import db from "db/db";
+import { TestSuiteTable } from "db/schema";
+import { count, isNull } from "drizzle-orm";
 import { Hono } from "hono";
 
 const DashboardRoute = new Hono();
@@ -6,9 +8,19 @@ DashboardRoute.get("/", async (ctx) => {
   const projects = await db.query.ProjectTable.findMany({
     where: (clm, { isNull }) => isNull(clm.deletedAt),
     orderBy: (clm, { desc }) => desc(clm.id),
+    with: {
+      listTestSuite: {
+        where: (clm, { isNull }) => isNull(clm.deletedAt),
+      },
+    },
   });
+
+  let totalTestsuite = 0;
   const dataTable = [];
   for (const item of projects) {
+    if (item.listTestSuite.length > 0) {
+      totalTestsuite += item.listTestSuite.length;
+    }
     dataTable.push({
       id: item.id,
       title: item.title,
@@ -19,7 +31,7 @@ DashboardRoute.get("/", async (ctx) => {
   }
   return ctx.json({
     totalProject: projects.length,
-    totalTestcase: 0,
+    totalTestsuite,
     totalAvgSuccessRate: 0,
     totalActive: 0,
     dataTable,
