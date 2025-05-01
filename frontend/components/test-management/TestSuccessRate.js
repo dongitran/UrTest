3501e42c07bed1a5d479d145523727216aa747ae
hieudien14310 <tranhieu14310@gmail.com>
2025-05-01 17:36:39 +0700

    feat: implement TestResourceModal and integrate with TestSuccessRate component

--- frontend/components/test-management/TestSuccessRate.js
+++ frontend/components/test-management/TestSuccessRate.js
@@ -1,102 +1,73 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import TestResourceModal from "@/components/test-management/TestResourceModal";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { TestResourceApi } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { ChevronLeft, ChevronRight, Edit, Trash2 } from "lucide-react";
 
-export default function TestSuccessRate() {
+export default function TestSuccessRate({ project = {} }) {
+  const [openModal, setOpenModal] = useState(false);
+  const { data, refetch } = useQuery({
+    enabled: project.id ? true : false,
+    queryKey: ["test-resource"],
+    queryFn: () => {
+      return TestResourceApi().list({ projectId: project.id });
+    },
+  });
+  console.log("data :>> ", data);
   return (
     <Card>
-      <CardHeader className="pb-2">
-        <div className="flex justify-between items-center">
-          <CardTitle>Test Success Rate</CardTitle>
-          <Select defaultValue="7days">
-            <SelectTrigger className="w-[130px] h-8">
-              <SelectValue placeholder="Last 7 days" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="7days">Last 7 days</SelectItem>
-              <SelectItem value="30days">Last 30 days</SelectItem>
-              <SelectItem value="90days">Last 90 days</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
+      <CardHeader className="">
+        <CardTitle className="flex gap-3 items-center">
+          <span className="text-xl font-semibold">Test Resource</span>
+          <div className="ml-auto"></div>
+          <TestResourceModal
+            dialogChild={<Button className="">Create Test Resource</Button>}
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+            projectId={project.id}
+            refetch={refetch}
+          />
+        </CardTitle>
+        <CardDescription>
+          Là các file chứa nội dung như các config, function chung để các Testsuite trong cùng Project có thể sử dụng
+        </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="space-y-4">
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col">
-              <div className="text-sm font-medium">Authentication</div>
-              <div className="text-xs text-muted-foreground">95%</div>
-            </div>
-            <div className="w-2/3">
-              <div className="h-2 w-full rounded-full bg-muted">
-                <div
-                  className="h-full rounded-full bg-green-500"
-                  style={{ width: "95%" }}
-                />
+        <div className="grid grid-cols-1 gap-3">
+          {data?.listTestResource.map((item) => {
+            return (
+              <div className="flex gap-3 items-center">
+                <div>
+                  <p className="text-lg font-semibold">{item.title}</p>
+                  <div className="max-w-80 text-xs text-muted-foreground truncate">{item.description}</div>
+                </div>
+                <div className="ml-auto"></div>
+                <div>
+                  <Button className="h-8 w-8" variant="ghost">
+                    <Edit className="size-4" />
+                  </Button>
+                  <Button variant="ghost" className="h-8 w-8 text-red-700 hover:text-red-800">
+                    <Trash2 className="size-4" />
+                  </Button>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col">
-              <div className="text-sm font-medium">Search</div>
-              <div className="text-xs text-muted-foreground">75%</div>
-            </div>
-            <div className="w-2/3">
-              <div className="h-2 w-full rounded-full bg-muted">
-                <div
-                  className="h-full rounded-full bg-yellow-500"
-                  style={{ width: "75%" }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col">
-              <div className="text-sm font-medium">Product</div>
-              <div className="text-xs text-muted-foreground">90%</div>
-            </div>
-            <div className="w-2/3">
-              <div className="h-2 w-full rounded-full bg-muted">
-                <div
-                  className="h-full rounded-full bg-green-500"
-                  style={{ width: "90%" }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col">
-              <div className="text-sm font-medium">Cart</div>
-              <div className="text-xs text-muted-foreground">60%</div>
-            </div>
-            <div className="w-2/3">
-              <div className="h-2 w-full rounded-full bg-muted">
-                <div
-                  className="h-full rounded-full bg-yellow-500"
-                  style={{ width: "60%" }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <div className="flex flex-col">
-              <div className="text-sm font-medium">Checkout</div>
-              <div className="text-xs text-muted-foreground">85%</div>
-            </div>
-            <div className="w-2/3">
-              <div className="h-2 w-full rounded-full bg-muted">
-                <div
-                  className="h-full rounded-full bg-green-500"
-                  style={{ width: "85%" }}
-                />
-              </div>
-            </div>
-          </div>
+            );
+          })}
+        </div>
+        <div className="flex items-center gap-1 mt-3 justify-end">
+          <Button variant="outline" size="icon" className="h-7 w-7">
+            <ChevronLeft className="size-4" />
+          </Button>
+          {[1, 2, 3, 4, 5].map((page) => (
+            <Button key={page} variant={1 === page ? "default" : "outline"} size="icon" className="h-7 w-7">
+              {page}
+            </Button>
+          ))}
+          <Button variant="outline" size="icon" className="h-7 w-7">
+            <ChevronRight className="size-4" />
+          </Button>
         </div>
       </CardContent>
     </Card>
