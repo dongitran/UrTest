8a8a33f2676b4fe79b4d5efcee23557a0af5f72f
hieudien14310 <tranhieu14310@gmail.com>
2025-05-02 22:43:33 +0700

    refactor: improve formatting and readability of CreateOrUpdateFile function

--- backend/src/lib/Github/CreateOrUpdateFile.ts
+++ backend/src/lib/Github/CreateOrUpdateFile.ts
@@ -16,19 +16,22 @@ export default async function CreateOrUpdateFile(
   },
   callback?: (data: Record<string, any>) => void
 ): Promise<void> {
-  const response = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${fileName}`, {
-    method: "PUT",
-    headers: {
-      Authorization: `Bearer ${Bun.env.GITHUB_TOKEN}`,
-      Accept: "application/vnd.github.v3+json",
-    },
-    body: JSON.stringify({
-      message: `${sha ? "Update" : "Create"} ${fileName} testsuite file`,
-      content: Buffer.from(fileContent).toString("base64"),
-      branch: "main",
-      sha, //? Nếu không có SHA thì là tạo mới, có thì là update
-    }),
-  });
+  const response = await fetch(
+    `${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${fileName}`,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${Bun.env.GITHUB_TOKEN}`,
+        Accept: 'application/vnd.github.v3+json',
+      },
+      body: JSON.stringify({
+        message: `${sha ? 'Update' : 'Create'} ${fileName} testsuite file`,
+        content: Buffer.from(fileContent).toString('base64'),
+        branch: 'main',
+        sha, //? Nếu không có SHA thì là tạo mới, có thì là update
+      }),
+    }
+  );
 
   const data = await response.json();
   if (callback) callback(data);
