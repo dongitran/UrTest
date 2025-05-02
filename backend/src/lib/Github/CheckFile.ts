360527f10595b96521f0c061db2d7b167e60330e
hieudien14310 <tranhieu14310@gmail.com>
2025-05-02 12:03:23 +0700

    refactor: update parameter naming in CheckFile and DeleteFile functions for consistency

--- backend/src/lib/Github/CheckFile.ts
+++ backend/src/lib/Github/CheckFile.ts
@@ -3,14 +3,8 @@
  * @param param0
  * @returns
  */
-export default async function CheckFileFromGithub({
-  projectSlug,
-  fileName,
-}: {
-  projectSlug: string;
-  fileName: string;
-}) {
-  const checkRes = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${fileName}`, {
+export default async function CheckFileFromGithub({ projectSlug, path }: { projectSlug: string; path: string }) {
+  const checkRes = await fetch(`${Bun.env.GITHUB_URTEST_WORKFLOW_API}/contents/tests/${projectSlug}/${path}`, {
     headers: {
       Authorization: `Bearer ${Bun.env.GITHUB_TOKEN}`,
       Accept: "application/vnd.github.v3+json",
